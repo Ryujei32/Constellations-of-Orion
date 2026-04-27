530f44092ff77fb351adf0ca59745519e3a733fb
Ryujei <107782455+Ryujei32@users.noreply.github.com>
2026-04-27 10:44:30 -0300

    Add files via upload

---
+++ NPCs/Bosses/Astraeus/CosmicAstraeus.cs
@@ -0,0 +1,255 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ConstellationsOfOrion.Content.NPCs.Bosses.Astraeus
+{
+    [AutoloadBossHead]
+    public class CosmicAstraeus : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[Type] = 4;
+
+            NPCID.Sets.MPAllowedEnemies[Type] = true;
+            NPCID.Sets.SpecificDebuffImmunity[Type][BuffID.Poisoned] = true;
+            NPCID.Sets.BossBestiaryPriority.Add(Type);
+
+            NPCID.Sets.NPCBestiaryDrawModifiers drawMods = new()
+            {
+                PortraitScale = 0.2f,
+                PortraitPositionYOverride = 0,
+                PortraitPositionXOverride = 0
+            };
+            NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, drawMods);
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.aiStyle = NPCAIStyleID.Slime;
+            NPC.width = 150;
+            NPC.height = 80;
+            NPC.alpha = 40;
+            NPC.lifeMax = 30000;
+
+            NPC.damage = 111;
+            NPC.defense = 30;
+            NPC.knockBackResist = 0f;
+
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            if (!Main.dedServ)
+            {
+                Music = MusicID.Boss2;
+                if (!Main.swapMusic == Main.drunkWorld && !Main.remixWorld)
+                {
+                    Music = MusicID.OtherworldlyBoss2;
+                }
+            }
+
+            NPC.noGravity = false;
+            NPC.noTileCollide = false;
+
+            NPC.boss = true;
+            NPC.npcSlots = 5f;
+            NPC.SpawnWithHigherTime(30);
+        }
+
+        public override void ModifyNPCLoot(NPCLoot npcLoot)
+{
+	// ⭐ MASTER MODE RELIC (100% in Master Mode only)
+	npcLoot.Add(ItemDropRule.MasterModeCommonDrop(
+		ModContent.ItemType<Content.Items.Placeables.Relics.CosmicAstraeusRelic>()
+	));
+
+	// ⭐ MASTER MODE PET (RARE)
+	npcLoot.Add(ItemDropRule.MasterModeDropOnAllPlayers(
+		ModContent.ItemType<Content.Items.Pets.StarrySundae>(),
+		4 // 1 in 4 chance (adjust if you want rarer)
+	));
+
+	// ⭐ RARE WEAPON DROP (Gelatinous Spray)
+	npcLoot.Add(ItemDropRule.Common(
+		ModContent.ItemType<Content.Items.Weapons.GelatinousSpray>(),
+		5 // 1 in 5 chance
+	));
+    
+    npcLoot.Add(ItemDropRule.Common(
+		ModContent.ItemType<Content.Items.Weapons.StellarLash>(),
+		5 // 1 in 5 chance
+	));
+
+	// ⭐ CONSTELLITE BAR DROP (RANDOM STACK)
+	npcLoot.Add(ItemDropRule.Common(
+		ModContent.ItemType<Content.Items.Materials.ConstelliteBar>(),
+		1,
+		18,
+		32
+	));
+
+	// ⭐ GEL DROP (RANDOM STACK)
+	npcLoot.Add(ItemDropRule.Common(
+		ItemID.Gel,
+		1,
+		40,
+		80
+	));
+}
+
+
+        public override bool CanHitPlayer(Player target, ref int cooldown)
+        {
+            cooldown = ImmunityCooldownID.Bosses;
+            return true;
+        }
+
+
+
+        private uint animation;
+        private uint animationTimer;
+        private uint superJumpTimer;
+        private uint SunBeamTimer;
+        private uint starTimer;
+        private float targetRotation = 0;
+        private Vector2 freeze;
+
+        public override void OnSpawn(IEntitySource source)
+        {
+            animation = 0;
+            animationTimer = 10;
+            superJumpTimer = (uint)Main.rand.Next(100, 200);
+            SunBeamTimer = (uint)Main.rand.Next(300, 480);
+            starTimer = (uint)Main.rand.Next(3, 7);
+        }
+
+        public override void AI()
+        {
+            NPC.TargetClosest(false);
+            Lighting.AddLight(NPC.Center, new Vector3(2f, 1f, 2f));
+            bool onGround = NPC.collideY;
+
+            // MainAnimation
+            if (onGround)
+            {
+                if (animationTimer == 0)
+                {
+                    animationTimer = 10;
+                    NPC.frame.Y += NPC.frame.Height;
+                }
+                NPC.frame.Y %= NPC.frame.Height * Main.npcFrameCount[Type];
+                animationTimer--;
+            }
+            else NPC.frame.Y = NPC.frame.Height * 2;
+
+            if (NPC.HasValidTarget)
+            {
+                NPC.immortal = false;
+
+                // SunBeam
+                if (NPC.life <= NPC.lifeMax / 2f)
+                {
+                    if (SunBeamTimer >= 150) targetRotation = (Main.player[NPC.target].Center - NPC.Center).ToRotation();
+                    if (SunBeamTimer >= 120 && SunBeamTimer < 240)
+                    {
+                        freeze = NPC.velocity;
+                        for (int i = 0; i < 100; i++)
+                        {
+                            float s = 1f - (SunBeamTimer - 120f) / 120;
+                            Vector2 rot = new(MathF.Cos(targetRotation), MathF.Sin(targetRotation));
+                            Dust dust = Dust.NewDustPerfect(NPC.Center + rot * (80f + Main.rand.Next(0, 3000)), DustID.GemTopaz, rot * Main.rand.NextFloat(1, 3), 0, default, s * 0.5f);
+                            dust.noGravity = true;
+                        }
+                    }
+                    if (SunBeamTimer < 120)
+                    {
+                        NPC.velocity = Vector2.Zero;
+                    }
+                    if (SunBeamTimer == 119)
+                    {
+                        int type = ModContent.ProjectileType<SunBeam>();
+                        for (int i = 0; i < 40; i++) Projectile.NewProjectile(null, NPC.Center, Vector2.Zero, type, NPC.damage * 2, 6f, -1, NPC.whoAmI, i, targetRotation);
+                    }
+                    if (SunBeamTimer == 0)
+                    {
+                        NPC.velocity = freeze;
+                        SunBeamTimer = (uint)Main.rand.Next(180, 300);
+                    }
+                    SunBeamTimer--;
+                }
+
+                // SuperJump
+                if (superJumpTimer == 0 && onGround)
+                {
+                    superJumpTimer = (uint)Main.rand.Next(200, 400);
+                    NPC.velocity.Y -= 12;
+                }
+                superJumpTimer--;
+
+                // FallenStars
+                if (starTimer == 0)
+                {
+                    starTimer = (uint)Main.rand.Next(3, 7);
+                    int type = ModContent.ProjectileType<FallenCrystalstar>();
+                    Vector2 position = NPC.Center + new Vector2(Main.rand.Next(-1000, 1000), -1000);
+                    Projectile.NewProjectile(null, position, Vector2.Zero, type, NPC.damage / 2, 6f);
+                }
+                starTimer--;
+            }
+            else NPC.immortal = true;
+            animation++;
+        }
+
+        public override void OnKill()
+        {
+        }
+
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return (NPC.color == default ? Color.White : NPC.color) * (1 - NPC.alpha / 255f);
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            float value = MathF.Sin(animation / 300f * MathF.PI * 2f) + 1f / 2f;
+            Texture2D texture = ModContent.Request<Texture2D>(Texture).Value;
+
+            // Star Spectral
+            Vector2 origin = NPC.frame.Size() * new Vector2(0.5f, 0.9f);
+            Vector2 position = NPC.position + new Vector2(NPC.width / 2f, NPC.height) - screenPos;
+            Color color1 = Color.Magenta * 0.3f * (1 - value * 0.6f);
+            Color color2 = Color.Pink * 0.4f * (1 - value * 0.5f);
+            Color color3 = Color.Gold * 0.5f * (1 - value * 0.5f);
+            float scale1 = NPC.scale * (value * 0.3f + 1.3f);
+            float scale2 = NPC.scale * (value * 0.2f + 1.2f);
+            float scale3 = NPC.scale * (value * 0.1f + 1.1f);
+            spriteBatch.Draw(texture, position, NPC.frame, color1, 0f, origin, scale1, SpriteEffects.None, 0f);
+            spriteBatch.Draw(texture, position, NPC.frame, color2, 0f, origin, scale2, SpriteEffects.None, 0f);
+            spriteBatch.Draw(texture, position, NPC.frame, color3, 0f, origin, scale3, SpriteEffects.None, 0f);
+            return true;
+        }
+
+        public override void PostDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            Texture2D textureSunRing = ModContent.Request<Texture2D>(Texture + "_SunRing").Value;
+
+            // Sun Ring
+            if (NPC.life <= NPC.lifeMax / 2f && SunBeamTimer < 150 && NPC.HasValidTarget)
+            {
+                Vector2 position = NPC.Center - screenPos + new Vector2(MathF.Cos(targetRotation), MathF.Sin(targetRotation)) * 50f;
+                float rotation = animation / 150f * MathF.PI * 2f;
+                float alpha = MathF.Sin(SunBeamTimer / 150f * MathF.PI);
+                float scale = NPC.scale * (alpha * 1.3f);
+                Rectangle frame = new(0, 0, 150, 150); Rectangle frame2 = new(150, 0, 14, 32); Vector2 origin = new(75, 75); Vector2 origin2 = new(7, 74f + alpha * 20f);
+                spriteBatch.Draw(textureSunRing, position, frame, Color.Yellow * alpha, rotation, origin, scale * 1.01f, SpriteEffects.None, 0f);
+                for (int i = 0; i < 16; i++) spriteBatch.Draw(textureSunRing, position, frame2, Color.Yellow * alpha, rotation + MathF.PI / 8f * i, origin2, scale * 1.01f, SpriteEffects.None, 0f);
+                spriteBatch.Draw(textureSunRing, position, frame, Color.White * alpha, rotation, origin, scale, SpriteEffects.None, 0f);
+                for (int i = 0; i < 16; i++) spriteBatch.Draw(textureSunRing, position, frame2, Color.White * alpha, rotation + MathF.PI / 8f * i, origin2, scale, SpriteEffects.None, 0f);
+            }
+        }
+    }
+}
---
+++ Tiles/Relics/CosmicAstraeusRelicTile.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace ConstellationsOfOrion.Content.Tiles.Relics
+{
+	public class CosmicAstraeusRelicTile : ModTile
+	{
+		public const int FrameWidth = 18 * 3;
+		public const int FrameHeight = 18 * 4;
+
+		public Asset<Texture2D> RelicTexture;
+
+		// ⭐ THIS POINTS TO YOUR FLOATING HEAD SPRITE
+		public virtual string RelicTextureName => "ConstellationsOfOrion/Content/Tiles/Relics/CosmicAstraeusRelic";
+
+		// ⭐ THIS IS YOUR PEDESTAL TILE
+		public override string Texture => "ConstellationsOfOrion/Content/Tiles/Relics/CosmicAstraeusRelicTile";
+
+		public override void Load()
+		{
+			RelicTexture = ModContent.Request<Texture2D>(RelicTextureName);
+		}
+
+		public override void SetStaticDefaults()
+		{
+			Main.tileShine[Type] = 400;
+			Main.tileFrameImportant[Type] = true;
+			TileID.Sets.InteractibleByNPCs[Type] = true;
+
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x4);
+			TileObjectData.newTile.LavaDeath = false;
+			TileObjectData.newTile.DrawYOffset = 2;
+			TileObjectData.newTile.Direction = TileObjectDirection.PlaceLeft;
+
+			TileObjectData.newTile.StyleHorizontal = false;
+
+			TileObjectData.newTile.StyleWrapLimitVisualOverride = 2;
+			TileObjectData.newTile.StyleMultiplier = 2;
+			TileObjectData.newTile.StyleWrapLimit = 2;
+			TileObjectData.newTile.styleLineSkipVisualOverride = 0;
+
+			TileObjectData.newAlternate.CopyFrom(TileObjectData.newTile);
+			TileObjectData.newAlternate.Direction = TileObjectDirection.PlaceRight;
+			TileObjectData.addAlternate(1);
+
+			TileObjectData.addTile(Type);
+
+			AddMapEntry(new Color(233, 207, 94), Language.GetText("MapObject.Relic"));
+		}
+
+		public override bool CreateDust(int i, int j, ref int type)
+		{
+			return false;
+		}
+
+		public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY)
+		{
+			tileFrameX %= FrameWidth;
+			tileFrameY %= FrameHeight * 2;
+		}
+
+		public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref TileDrawInfo drawData)
+		{
+			if (drawData.tileFrameX % FrameWidth == 0 && drawData.tileFrameY % FrameHeight == 0)
+			{
+				Main.instance.TilesRenderer.AddSpecialPoint(i, j, Terraria.GameContent.Drawing.TileDrawing.TileCounterType.CustomNonSolid);
+			}
+		}
+
+		public override void SpecialDraw(int i, int j, SpriteBatch spriteBatch)
+		{
+			Point p = new Point(i, j);
+			Tile tile = Main.tile[p.X, p.Y];
+
+			if (!tile.HasTile)
+				return;
+
+			Texture2D texture = RelicTexture.Value;
+
+			Rectangle frame = texture.Frame();
+
+			Vector2 origin = frame.Size() / 2f;
+			Vector2 worldPos = p.ToWorldCoordinates(24f, 64f);
+
+			Color color = Lighting.GetColor(p.X, p.Y);
+
+			bool direction = tile.TileFrameY / FrameHeight != 0;
+			SpriteEffects effects = direction ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+
+			const float TwoPi = (float)Math.PI * 2f;
+
+			float offset = (float)Math.Sin(Main.GlobalTimeWrappedHourly * TwoPi / 5f);
+
+			Vector2 drawPos = worldPos - Main.screenPosition +
+				new Vector2(0f, -40f + offset * 4f);
+
+			// ⭐ DRAW HEAD
+			spriteBatch.Draw(texture, drawPos, frame, color, 0f, origin, 1f, effects, 0f);
+
+			// ⭐ GLOW EFFECT
+			float scale = (float)Math.Sin(Main.GlobalTimeWrappedHourly * TwoPi / 2f) * 0.3f + 0.7f;
+
+			Color glow = color;
+			glow.A = 0;
+			glow *= 0.2f * scale;
+
+			for (float i2 = 0; i2 < 1f; i2 += 0.25f)
+			{
+				Vector2 offsetGlow = (TwoPi * i2).ToRotationVector2() * 6f;
+				spriteBatch.Draw(texture, drawPos + offsetGlow, frame, glow, 0f, origin, 1f, effects, 0f);
+			}
+		}
+	}
+}
